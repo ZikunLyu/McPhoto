a89223fc0a5dd97690c740835a969859f0118bc2
Wenhao <owengwh123@gmail.com>
2020-02-10 21:19:18 -0500

    Connect to online Mongo Atlas database

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("Database connection successful!");
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {

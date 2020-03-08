2821044cde7006cb16b71d4f933e37d64ed38122
Wenhao <owengwh123@gmail.com>
2020-03-08 19:46:11 -0400

    More error handling setup

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,53 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+}
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error, send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } else {
+    // Programming or other unknown error (eg. from 3rd party package), don't leak error details
+    // 1) Log error
+    console.log('Error 🔥', err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!'
+    });
+  }
+}
+
 // Why all API related error will be directed to here?
 // Cuz the API call definitely respond req, res, and next, if there is an error,
 // the err param will also be added, thus knowing it should trigger the function here
@@ -5,8 +55,21 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    // it's a good practise to not directly mutate the returned err, but create a copy of it to be modified.
+    let error = { ...err };
+    console.log(error.code);
+    // In the production, as we defined in the sendErrorProd(), 
+    // we concise the error msg, but also in the case of third party error, we don't throw anything
+    // MongoDB side error will be considered as 3rd party, however, we do want the error to be thrown to user if it's coming from MongoDB
+    // Thus, we have following 3 if statements to specially take care of mongoDB errors
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+
+    sendErrorProd(error, res);
+  }
 };

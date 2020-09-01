bf98d4254fe57ffcc852d6ae76831341c8595c5a
Wenhao <owengwh123@gmail.com>
2020-08-31 18:42:38 -0600

    Get user profile API

---
+++ controllers/userController.js
@@ -0,0 +1,23 @@
+const User = require('./../models/userModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+exports.getAUser = catchAsync(async (req, res, next) => {
+  const { email } = req.body;
+
+  if (!email) {
+    return next(new AppError('Please provide email to get User object.', 400));
+  }
+
+  const user = await User.findOne({ email: email });
+
+  if (!user) {
+    return next(new AppError('User with specified email does not exist', 401));
+  }
+
+  // 3) If everything is ok, send the token to the user
+  res.status(200).json({
+    status: 'success',
+    user
+  });
+});
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const authController = require('./../controllers/authController');
+const userController = require('./../controllers/userController');
 
 const router = express.Router();
 
@@ -12,5 +13,7 @@ router.patch(
   authController.protect,
   authController.updatePassword
 );
+
+router.get('/getAUser', userController.getAUser);
 
 module.exports = router;

f7962b34f0deb2172f3ca68418bc81de3cfa5af1
Wenhao <owengwh123@gmail.com>
2020-11-19 22:20:32 -0500

    Allow all CORS methods

--- app.js
+++ app.js
@@ -67,7 +67,10 @@ app.use((req, res, next) => {
 // Allow CORS
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET,HEAD,OPTIONS,POST,PUT,DELETE,CONNECT,TRACE,PATCH'
+  );
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
